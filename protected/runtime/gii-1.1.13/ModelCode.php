917b0f32c8604e8a01e2de53f698e01c18f5f319
cuongdo <cuongdd21@gmail.com>
2013-05-10 15:52:23 +1000

    add more files

---
+++ protected/runtime/gii-1.1.13/ModelCode.php
@@ -0,0 +1,9 @@
+<?php
+return array (
+  'template' => 'default',
+  'connectionId' => 'db',
+  'tablePrefix' => 'tbl_staff',
+  'modelPath' => 'application.models',
+  'baseClass' => 'CActiveRecord',
+  'buildRelations' => '1',
+);
